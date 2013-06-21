c356be53264412b6154a95f2e6919fd9eb71958c
Ryan Duffy <ryan@tiqtech.com>
2013-06-21 10:53:57 -0400

    adds two-way property updates which requires enyo 2.3

--- enyo-polymer.js
+++ enyo-polymer.js
@@ -1,28 +1,51 @@
 (function registerKindAsWebComponent(enyo) {
     if(!enyo) return;
+    
+    function getPublished(ctor) {
+        if(ctor) {
+            return enyo.mixin(enyo.clone(ctor.prototype.published), getPublished(ctor.prototype.base));
+        } else {
+            return {};
+        }
+    }
 
     function featureHook(ctor, props) {
         var e = Object.create(HTMLElement.prototype);
         e.readyCallback = function() {
+            
+            // retrieve published enyo properties (including all inherited)
+            this.enyoPublished = getPublished(ctor);
+            
+            // filter node attributes by published
             var props = {};
             enyo.forEach(this.attributes, function(a) {
-                props[a.name] = a.value;
-            });
-
+                if(this.enyoPublished.hasOwnProperty(a.name)) {
+                    props[a.name] = a.value;
+                }
+            }, this);
+            
+            // create the enyo instance
             this.enyoInstance = new ctor(props);
+            
+            // wire a callback to update node attributes when published properties are changed
+            enyo.forEach(Object.keys(this.enyoPublished), function(prop) {
+                this.enyoInstance.addObserver(prop, function(property, prev, value) {
+                    this.setAttribute(property, this.enyoInstance[property]);
+                }, this);
+            }, this);
+            
+            // render the instance
             this.enyoInstance.renderInto(this);
-        }
+        };
 
+        // dispatch node attribute changes to the enyo instance
         e.attributeChangedCallback = function(name, oldValue) {
-            var me = this.enyoInstance;
-            var value = this.getAttribute(name);
-            //if(me.set) {    // >= v2.3
-            //    me.set(name, value);
-            //} else {        // < v2.3
-                me["set"+enyo.cap(name)](value);
-            //}
-        }
+            if(this.enyoPublished.hasOwnProperty(name)) {
+                this.enyoInstance.set(name, this.getAttribute(name));
+            }
+        };
 
+        // register the enyo kind as a web component
         document.register("x-"+ctor.prototype.kindName.replace(".", "-").toLowerCase(), {
             prototype: e
         });
